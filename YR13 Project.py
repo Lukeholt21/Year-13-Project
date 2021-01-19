69bba0eecc569c95f62d9a98b8c7b56548c1591d
Lukeholt21 <37617338+Lukeholt21@users.noreply.github.com>
2021-01-19 13:11:46 +0000

    Update YR13 Project.py

--- YR13 Project.py
+++ YR13 Project.py
@@ -1,4 +1,74 @@
 from tkinter import *
+#from tkinter import ttk
+
+def createUser():
+    
+    firstName_Pass = False
+    surname_Pass = False
+    sims_Pass = False
+    password_Pass = False
+
+    firstName = "Luke"
+    surname = "Holt"
+    sims = "lho"
+    password ="root"
+
+    while firstName_Pass == False:
+        if len(firstName) <=20 and len(firstName)>0:
+            firstName_Pass = True
+        else:
+            print("Please enter first name.")
+
+    while surname_Pass == False:
+        if len(surname) <= 20 and len(surname) >0:
+            surname_Pass = True
+        else:
+            print("Please enter surname.")
+
+    while sims_Pass == False:
+        if len(sims) == 3:
+            sims_Pass = True
+        else:
+            print("Please enter your sims code.")
+
+    while password_Pass == False:
+        if len(password) <= 24 and len(password)>3:
+            password_Pass = True
+        else:
+            print("Please enter a password that is between 4 and 24 characters long.")
+
+    createUser_Form = Toplevel(mainform)
+    createUser_Form.title("Create User")
+    createUser_Form.geometry("400x600")
+
+    firstName_lbl =Label(createUser_Form,text="First Name")
+    firstName_lbl.grid(column=0,row=0,padx=50)
+    surname_lbl = Label(createUser_Form,text="Surname")
+    surname_lbl.grid(column=0,row=1,padx=100)
+    sims_lbl = Label(createUser_Form,text="Sims Code")
+    sims_lbl.grid(column=0,row=2,padx=100)
+    password_lbl = Label(createUser_Form,text="Password")
+    password_lbl.grid(column=0,row=3,padx=100)
+
+    firstName = StringVar()
+    firstName_ety = Entry(createUser_Form,width=15, textvariable=firstName)
+    firstName_ety.grid(column=1,row=0,padx=10)
+    surname = StringVar()
+    surname_ety = Entry(createUser_Form,width=15, textvariable=surname)
+    surname_ety.grid(column=1,row=1,padx=10)
+    sims = StringVar()
+    sims_ety = Entry(createUser_Form,width=15, textvariable=sims)
+    sims_ety.grid(column=1,row=2,padx=10)
+    password = StringVar()
+    password_ety = Entry(createUser_Form,width=15,textvariable=password)
+    password_ety.grid(column=1,row=3,padx=10)
+
+    user_rad = Radiobutton(createUser_Form,text="User")
+    user_rad.grid(column=0,row=0)
+    admin_rad = Radiobutton(createUser_Form,text="Admin")
+    admin_rad.grid(column=1,row=1)
+
+    create_but = Button(createUser_Form,text="Create",command=None)
 
 def teacherLogin():
     teacherlogin_Form = Toplevel(mainForm)
@@ -21,7 +91,6 @@ def teacherLogin():
 
 def adminLogin():
     def Login_command():
-        None
 
         counter = 0
         Valid = False
@@ -48,6 +117,7 @@ def adminLogin():
     password_ety.grid(column=1,row=2,padx=10)
 
     login_but =Button(adminLogin_Form,text="Login",command= Login_command)
+    login_but.grid(column=1,row=3)
 
 def viewTimetables():
     None
