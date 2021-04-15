5272e2589bbfd2b0024ff017d175c2c6eb6b9d8b
Lukeholt21 <37617338+Lukeholt21@users.noreply.github.com>
2021-04-15 11:24:03 +0100

    Add files via upload

--- YR13 Project.py
+++ YR13 Project.py
@@ -11,7 +11,7 @@ def createUser():
         password_Pass = False
 
 
-        print(firstName.get(),surname.get(),sims.get(),password.get(),userType.get())
+        #print(firstName.get(),surname.get(),sims.get(),password.get(),userType.get())
 
 
         if firstName_Pass == False:
@@ -72,8 +72,15 @@ def createUser():
             simsCode = sims.get()
             accessLevel = userType.get()
 
+            print(userID,firstName,surname,simsCode,accessLevel)
             from Save_Load import saveUser
             saveUser(userID,firstName,surname,simsCode,accessLevel)
+            if accessLevel == 1:
+                usrType = "User"
+            elif accessLevel == 2:
+                usrType = "Admin"
+            messagebox.showinfo("New User Information",("User ID: ",userID,"Name: ",firstName,surname,"Sims Code: ",simsCode,"User Type: ",usrType))
+            createUser_Form.destroy()
 
 
 
@@ -114,56 +121,41 @@ def createUser():
     create_but = Button(createUser_Form,text="Create",command=lambda:check_Create(firstName,surname,sims,password,userType))
     create_but.grid(column=0,row=5)
 
-
-
-
-def teacherLogin():
-    teacherlogin_Form = Toplevel(mainForm)
-    teacherlogin_Form.title("Teacher Login")
-    teacherlogin_Form.geometry("600x150")
-
-    title_lbl = Label(teacherlogin_Form,text="Login")
-    title_lbl.grid(column=1,row=0,padx=50)
-    userID_lbl = Label(teacherlogin_Form,text="User ID")
-    userID_lbl.grid(column=0,row=1,padx=100)
-    password_lbl = Label(teacherlogin_Form,text="Password")
-    password_lbl.grid(column=0,row=2,padx=100)
-    
-    userID = StringVar()
-    userID_ety = Entry(teacherlogin_Form,width=15, textvariable=userID)
-    userID_ety.grid(column=1,row=1,padx=10)
-    password = StringVar()
-    password_ety = Entry(teacherlogin_Form,width=15,textvariable=password)
-    password_ety.grid(column=1,row=2,padx=10)
-
-def adminLogin():
+def Login():
     def Login_command():
+        from Save_Load import loadUsers
+        Users = loadUsers()
+        if len(Users) ==0:
+            messagebox.showerror("ERROR","No users detected.")
+        from Classes import userClass
+        #counter = 0
+        #Valid = False
+        #while counter < 4:
+        for i in range(0,len(Users)):
+            if userID_inp.get() == Users[i].userID:
+                usrFound = True
+                if password_inp == Users[i].password:
+                    print("User Verified")
 
-        counter = 0
-        Valid = False
-        while counter < 4:
-            for userID in users:
-                None
+    Login_Form = Toplevel(mainForm)
+    Login_Form.title("Administrator Login")
+    Login_Form.geometry("600x150")
 
-    adminLogin_Form = Toplevel(mainForm)
-    adminLogin_Form.title("Administrator Login")
-    adminLogin_Form.geometry("600x150")
-
-    title_lbl =Label(adminLogin_Form,text="Login")
+    title_lbl =Label(Login_Form,text="Login")
     title_lbl.grid(column=1,row=0,padx=50)
-    userID_lbl = Label(adminLogin_Form,text="User ID")
+    userID_lbl = Label(Login_Form,text="User ID")
     userID_lbl.grid(column=0,row=1,padx=100)
-    password_lbl = Label(adminLogin_Form,text="Password")
+    password_lbl = Label(Login_Form,text="Password")
     password_lbl.grid(column=0,row=2,padx=100)
 
-    userID = StringVar()
-    userID_ety = Entry(adminLogin_Form,width=15, textvariable=userID)
+    userID_inp = StringVar()
+    userID_ety = Entry(Login_Form,width=15, textvariable=userID_inp)
     userID_ety.grid(column=1,row=1,padx=10)
-    password = StringVar()
-    password_ety = Entry(adminLogin_Form,width=15,textvariable=password)
+    password_inp = StringVar()
+    password_ety = Entry(Login_Form,width=15,textvariable=password_inp)
     password_ety.grid(column=1,row=2,padx=10)
 
-    login_but =Button(adminLogin_Form,text="Login",command= Login_command)
+    login_but =Button(Login_Form,text="Login",command= Login_command)
     login_but.grid(column=1,row=3)
 
 def viewTimetables():
@@ -171,19 +163,18 @@ def viewTimetables():
 
 mainForm = Tk()
 mainForm.title("Booking Login Screen")
-mainForm.geometry("600x300")
+mainForm.geometry("450x300")
 
 welcome_lbl = Label(mainForm,text="Welcome!")
-welcome_lbl.grid(column=1,row=0,pady=20)
-teacherLogin_but = Button(mainForm,text="Teacher Login", command= teacherLogin )
-teacherLogin_but.grid(column=0,row=1,padx=50,pady=100)
-adminLogin_but =Button(mainForm,text="Admin Login",command=adminLogin)
-adminLogin_but.grid(column=1,row=1,padx=50,pady=100)
+welcome_lbl.grid(column=1,row=0,pady=20,padx=25)
+#teacherLogin_but = Button(mainForm,text="Teacher Login", command= teacherLogin )
+#teacherLogin_but.grid(column=0,row=1,padx=50,pady=100)
+Login_but =Button(mainForm,text="Login",command=Login)
+Login_but.grid(column=0,row=1,padx=50,pady=100)
 viewTimetables_but = Button(mainForm,text="View Time Tables",command=viewTimetables)
-viewTimetables_but.grid(column=2,row=1,padx=50,pady=100)
+viewTimetables_but.grid(column=2,row=1,pady=100)
 #mainForm.iconbitmap("Logo.ico")
 
-createUser()
 
 mainForm.mainloop()
 
